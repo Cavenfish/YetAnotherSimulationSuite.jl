7d993dbf15bd2e65813c91f18f5c420a7077a33c
brian <ferraribrian1996@gmail.com>
2023-05-30 20:03:12 +0200

    did some minor things

--- src/md/simulation.jl
+++ src/md/simulation.jl
@@ -7,12 +7,12 @@ struct Simulation
   energy::Float64
 end
 
-function run_NVE(EoM, tspan, dt, algo, bdys)
+function run_NVE(EoM, tspan, dt, algo, bdys; kwargs...)
   pos   = [i.r for i in bdys]
   vel   = [i.v for i in bdys]
   m     = [i.m for i in bdys]
 
-  prob  = SecondOrderODEProblem(EoM, vel, pos, tspan, m)
+  prob  = SecondOrderODEProblem(EoM, vel, pos, tspan, m; kwargs...)
   solu  = solve(prob, algo, dt=dt)
 
   return solu
