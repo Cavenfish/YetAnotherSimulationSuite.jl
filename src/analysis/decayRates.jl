87878f596beaf957723c950f738bf0dd7cbaa4a9
Brian C. Ferrari <ferraribrian1996@gmail.com>
2023-09-07 17:07:39 +0200

    another decay method

--- src/analysis/decayRates.jl
+++ src/analysis/decayRates.jl
@@ -5,6 +5,9 @@ twoChannel(t, p) = p[1] * (exp.(- t / p[2]) + exp.(-t / p[3]))
 #Exponential decay
 expDecay(t, p) = p[1] * exp.(-t / p[2]) .+ p[3]
 
+#Exponential decay with sin function for random gains
+expSinDecay(t, p) = p[1] * exp.(-t / p[2]) .+ (p[3] * sin.(p[4] * t))
+
 #Fit self dissipative cases
 function selfDisp(df)
 
@@ -19,4 +22,13 @@ function selfDisp(df)
   (ifit.param[2], jfit.param[2])
 end
 
-  
+function nonSelfDisp(df)
+
+  t = df.time[102:end] ./ 1000 .- 10
+  y = df.molVib[102:end]
+
+  afit = curve_fit(expDecay, t, y, [0.4, 500, 0.0])
+  # bfit = curve_fit(expSinDecay, t, y, [0.4, 500, 0.01, 0.0001])
+
+  afit.param[2]
+end
