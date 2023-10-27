916ddb9a4b69d4b08f3a30048b46f8079f9be306
Brian C. Ferrari <ferraribrian1996@gmail.com>
2023-10-27 17:37:58 +0200

    working on freqShifts code, still not working

--- src/analysis/freqShifts.jl
+++ src/analysis/freqShifts.jl
@@ -6,6 +6,21 @@ struct MolFreq
   r::Float64
 end
 
+function _getWave(bl)
+  aTol = bl[1] / 1e6
+  tmp  = findall(e -> isapprox(e, bl[1]; atol=aTol), bl[10:end])
+
+  while length(tmp) < 2
+    println(aTol)
+    aTol *= 1.2
+    tmp   = findall(e -> isapprox(e, bl[1]; atol=aTol), bl[10:end])
+  end
+  
+  wave = bl[1:tmp[2]+9] .- mean(bl[1:tmp[2]+9]) |> (x -> repeat(x, 500))
+
+  wave
+end
+
 function getMolFreq(EoM, tj, dt)
 
   ind = let
@@ -20,23 +35,25 @@ function getMolFreq(EoM, tj, dt)
   for i in 1:dt:length(tj.t)
 
     bdys = getFrame(tj, i)
-    Evib = getCOVibEnergy(byds[ind]; pot=EoM)
+    Evib = getCOVibEnergy(bdys[ind]; pot=EoM)
 
     nve  = runNVE(EoM, (0, 50fs), 0.1fs, bdys) |> (x -> processDynamics(x; dt=0.1fs))
     r    = [j[ind] for j in nve.r]
     bl   = [norm(j[2]-j[1]) for j in r]
 
-    tmp  = findall(e -> isapprox(e, bl[1]; atol=3e-4), bl[2:end])
-
-    wave = bl[1:tmp[2]] .- mean(bl[1:tmp[2]]) |> (x -> repeat(x, 500))
+    wave = _getWave(bl)
+    return wave
 
     n = div(length(wave), 2)
     I = abs.(fft(wave))
     v = fftfreq(length(I), 1e16) ./ 29979245800.0
 
-    pks = findPeaks(I[1:n]; min=1e3)
+    pks = findPeaks(I[1:n]; min=1e1)
 
-    push!(freqs, MolFreq(Evib, I[pks[1]], tj.t[i], 0.0))
+    length(pks) > 0 || continue
+    println(v[pks[1]])
+
+    push!(freqs, MolFreq(Evib, v[pks[1]], tj.t[i], 0.0))
   end
 
   freqs
