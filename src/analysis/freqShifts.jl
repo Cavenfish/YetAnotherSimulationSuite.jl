7fba684a1cb8c9a6b3fb502bb3ea8b1caafe6ec9
Brian C. Ferrari <ferraribrian1996@gmail.com>
2023-10-31 11:24:47 +0100

    minor edits

--- src/analysis/freqShifts.jl
+++ src/analysis/freqShifts.jl
@@ -6,17 +6,6 @@ struct MolFreq
   r::Float64
 end
 
-struct oneFreq
-  Evib::Float64
-  freq::Float64
-  r::Float64
-end
-
-struct AllFreq
-  time::Float64
-  freqs::Vector
-end
-
 function _getWave(bl)
   tmpi = findall(e -> isapprox(e, maximum(bl); atol=5e-5), bl)
   tmpj = findall(e -> 1000 < e - tmpi[1] < 2000, tmpi)
@@ -62,7 +51,6 @@ function getMolFreq(EoM, tj, dt)
     pks = findPeaks(I[1:n]; min=1e1)
 
     length(pks) > 0 || continue
-    println(v[pks[1]])
 
     push!(freqs, MolFreq(Evib, v[pks[1]], tj.t[i], 0.0))
   end
@@ -81,7 +69,6 @@ function getAllFreqs(EoM, tj, dt)
   end 
 
   allFreqs = MolFreq[]
-  # allFreqs = AllFreq[]
   for i in 1:dt:length(tj.t)
 
     bdys = getFrame(tj, i)
@@ -89,7 +76,6 @@ function getAllFreqs(EoM, tj, dt)
     mols = getMols(bdys)
     main = CoM(bdys[ind])
 
-    # freqs = oneFreq[]
     for mol in mols
       Evib = getCOVibEnergy(bdys[mol]; pot=EoM)
       r    = [j[mol] for j in nve.r]
@@ -113,9 +99,7 @@ function getAllFreqs(EoM, tj, dt)
       sub = CoM(bdys[mol])
       d   = norm(main - sub)
       push!(allFreqs, MolFreq(Evib, v[pks[1]], tj.t[i], d))
-      # push!(freqs, oneFreq(Evib, v[pks[1]], tj.t[i], d))
     end
-    # push!(allFreqs, AllFreq(tj.t[i], freqs))
   end
 
   allFreqs
