a2bf7dac43669759ea0356931741c7b053b59f6b
Brian C. Ferrari <ferraribrian1996@gmail.com>
2023-11-24 19:00:35 +0100

    adding some decay rate stuff

--- src/analysis/decayRates.jl
+++ src/analysis/decayRates.jl
@@ -3,7 +3,7 @@
 twoChannel(t, p) = (p[1] * exp.(- t / p[2])) .+ (p[3] * exp.(-t / p[4])) .+ abs(p[5])
 
 #Exponential decay
-expDecay(t, p) = p[1] * exp.(-t / p[2]) .+ abs(p[3])
+expDecay(t, p) = p[1] * exp.(-t / p[2]) #.+ abs(p[3])
 
 #Exponential decay with sin function for random gains
 expSinDecay(t, p) = p[1] * exp.(-t / p[2]) .+ (p[3] * sin.(p[4] * t))
@@ -11,7 +11,7 @@ expSinDecay(t, p) = p[1] * exp.(-t / p[2]) .+ (p[3] * sin.(p[4] * t))
 #My custom decay-rate with frequency gap law included
 function myDecay(t,p)
   τ =  p[1] * exp.(t / p[2])
-  p[3] * exp.(-t ./ τ) .+ p[4]
+  0.405 * exp.(-t ./ τ) .+ 0.01
  end
 
 function prep4fit(df)
@@ -30,6 +30,18 @@ function trackTau(df; N=100)
   τ   = []
   for i in 1:length(X)
     fit = curve_fit(expDecay, X[i], Y[i], [0.4, 500.0, 0.0])
+    push!(τ, fit.param[2])
+  end
+
+  τ
+end
+
+function trackTau(df, N)
+  t,y = prep4fit(df)
+
+  τ   = []
+  for i in 0:length(t)- N - 1
+    fit = curve_fit(expDecay, t[1+i:N+i], y[1+i:N+i], [0.4, 500.0])
     push!(τ, fit.param[2])
   end
 
