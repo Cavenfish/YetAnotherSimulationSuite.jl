981ca025ef1c2c9f7ef2d56f0c0eac4b91febb60
Brian C. Ferrari <ferraribrian1996@gmail.com>
2023-12-04 17:33:08 +0100

    forgot what changed

--- src/analysis/decayRates.jl
+++ src/analysis/decayRates.jl
@@ -21,21 +21,6 @@ function prep4fit(df)
   (t,y)
 end
 
-function trackTau(df; N=100)
-  t,y = prep4fit(df)
-
-  X   = Iterators.partition(t, N) |> collect
-  Y   = Iterators.partition(y, N) |> collect
-
-  τ   = []
-  for i in 1:length(X)
-    fit = curve_fit(expDecay, X[i], Y[i], [0.4, 500.0, 0.0])
-    push!(τ, fit.param[2])
-  end
-
-  τ
-end
-
 function trackTau(df, N)
   t,y = prep4fit(df)
 
@@ -48,26 +33,3 @@ function trackTau(df, N)
   τ
 end
 
-#Fit self dissipative cases
-function selfDisp(df; thresh=0.055)
-
-  t = df.time[102:end] ./ 1000 .- 10
-  y = df.molVib[102:end]
-  i = findall(e -> e > thresh, y)
-  j = findall(e -> e < thresh, y)
-
-  ifit = curve_fit(expDecay, t[i], y[i], [0.4, 500, 0.0])
-  jfit = curve_fit(expDecay, t[j], y[j], [0.1, 50, 0.01])
-
-  (ifit, jfit)
-end
-
-function nonSelfDisp(df)
-
-  t = df.time[102:end] ./ 1000 .- 10
-  y = df.molVib[102:end]
-
-  afit = curve_fit(expDecay, t, y, [0.4, 500, 1500, ])
-
-  afit
-end
