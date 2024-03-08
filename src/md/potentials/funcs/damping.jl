0e15d3d757a429fef64230e38108fc1a95cb3017
Brian C. Ferrari <ferraribrian1996@gmail.com>
2024-03-08 18:26:56 +0100

    add damping functions

---
+++ src/md/potentials/funcs/damping.jl
@@ -0,0 +1,16 @@
+"""
+Damping Functions for Potentials
+
+Notes:
+  - Functions will be passed to inter functions,
+    so they should follow the format foo(r, p).
+    Where p can be a single value or an array with
+    all additional damping parameters.
+"""
+
+function tangToennies(r, D)
+  dr = D * r
+  s  = [dr^i/factorial(i) for i = 0:6] |> sum
+  
+  1 - (exp(dr) * s)
+end
