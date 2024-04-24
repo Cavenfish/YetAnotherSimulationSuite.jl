dd4cb7c579d1b0ae7935d1f36384d71b05895c6f
Brian C. Ferrari <ferraribrian1996@gmail.com>
2024-04-24 20:08:18 +0200

    wanting to check CH4 pot now

--- src/md/potentials/CH4.jl
+++ src/md/potentials/CH4.jl
@@ -4,59 +4,90 @@ from Riera et al. 2020
 https://doi.org/10.1021/acs.jpcb.0c08728
 """
 
-struct _CH4_PotVars <: PotVars
+struct _CH4_PotVars{F<:Float64} <: PotVars
+  D::F
+  a::F
+  req::F
+  K::F
+  θeq::F
+  Acc::F
+  Ach::F
+  Ahh::F
+  Bcc::F
+  Bch::F
+  Bhh::F
+  Ccc::F
+  Cch::F
+  Chh::F
+  Qh::F
+  Qc::F
+  αh::F
+  αc::F
+  A6cc::F
+  A6ch::F
+  A6hh::F
   μ::Vector
+  α::Vector
+  Q::Vector
 end
 
 function CH4(bdys::Vector{Atom})
-  μ = [zeros(3) for i in bdys]
+  N  = div(length(bdys), 5)
+  Qh = 0.51163
+  Qc = -4Qh
+  αh = 0.38978
+  αc = 1.39327
+  μ  = [zeros(3) for i in bdys]
+  α  = repeat([αc, αh, αh, αh, αh], N)
+  Q  = repeat([Qc, Qh, Qh, Qh, Qh], N)
 
-  _CH4_PotVars(μ)
+  _CH4_PotVars(
+    4.0017,  # eV
+    2.0415,  # \AA^-1
+    1.0887,  # \AA
+    3.33487, # eV rad^-1   76.9061 * 0.043363
+    1.87411, # rad 107.379 * (2pi/360)
+    1852.25, #
+    141.317, #
+    112.503, #
+    3.37925, # \AA^-1
+    3.25885, # \AA^-1
+    4.05972, # \AA^-1
+    13.15,   #
+    4.51455, #
+    1.59497, #
+    Qh,      #
+    Qc,      #
+    αh,      # \AA^3
+    αc,      # \AA^3
+    αc^(2/6),
+    (αc*αh)^(1/6),
+    αh^(2/6),
+    μ
+  )
 end
 
 function CH4(dv, v, u, p, t)
-  D    = 4.0017  # eV
-  a    = 2.0415  # \AA^-1
-  req  = 1.0887  # \AA
-  K    = 3.33487 # eV rad^-1   76.9061 * 0.043363
-  θeq  = 1.87411 # rad 107.379 * (2pi/360)
-  Acc  = 1852.25 #
-  Ach  = 141.317 #
-  Ahh  = 112.503 #
-  Bcc  = 3.37925 # \AA^-1
-  Bch  = 3.25885 # \AA^-1
-  Bhh  = 4.05972 # \AA^-1
-  Ccc  = 13.15   #
-  Cch  = 4.51455 #
-  Chh  = 1.59497 #
-  Qh   = 0.51163 #
-  Qc   = - 4Qh   #
-  αh   = 0.38978 # \AA^3
-  αc   = 1.39327 # \AA^3
-  A6cc = αc^(2/6)
-  A6ch = (αc*αh)^(1/6)
-  A6hh = αh^(2/6)
 
   # initialize things
   E = 0.0
   F = zero(u)
-  α = repeat([αc, αh, αh, αh, αh], length(p.mols))
-  Q = repeat([Qc, Qh, Qh, Qh, Qh], length(p.mols))
+  P = p.potVars
 
-  _getDipoles4TTM_MatrixInversion!(p.μ, u, Q, α)
+  _getDipoles4TTM_MatrixInversion!(P.μ, u, P.Q, P.α)
 
-  E += _getDipolePolarizationEnergy(p.μ, α)
+  E += _getDipolePolarizationEnergy(P.μ, P.α)
 
   for mol in p.mols
     c, hs = mol[1], mol[2:5]
 
     for i in hs
-      E += _Morse!(F, u, c, i, D, a, req)
+      E += _Morse!(F, u, c, i, P.D, P.a, P.req)
     end
 
     for i in 1:4
       for j in i+1:4
-        E += _harmonicBondAngle!(F, u, hs[i], c, hs[j], K, θeq)
+        E += _harmonicBondAngle!(F, u, hs[i], c, hs[j], P.K, P.θeq)
       end
     end
     
@@ -67,25 +98,25 @@ function CH4(dv, v, u, p, t)
     c2, hs2 = par[2][1], par[2][2:5] 
 
     E += _interTTM!(
-      F, u, p.μ, c1, c2, Qc, Qc, 
-      Acc, Bcc, Ccc, A6cc; damp=tangToennies, p=Bcc
+      F, u, P.μ, c1, c2, P.Qc, P.Qc, 
+      P.Acc, P.Bcc, P.Ccc, P.A6cc; damp=tangToennies, p=P.Bcc
     )
 
     k = true
     for i in hs1
       E += _interTTM!(
-        F, u, p.μ, i, c2, Qh, Qc, 
-        Ach, Bch, Cch, A6ch; damp=tangToennies, p=Bch
+        F, u, P.μ, i, c2, P.Qh, P.Qc, 
+        P.Ach, P.Bch, P.Cch, P.A6ch; damp=tangToennies, p=P.Bch
       )
       for j in hs2
         E += _interTTM!(
-          F, u, p.μ, i, j, Qh, Qh, 
-          Ahh, Bhh, Chh, A6hh; damp=tangToennies, p=Bhh
+          F, u, P.μ, i, j, P.Qh, P.Qh, 
+          P.Ahh, P.Bhh, P.Chh, P.A6hh; damp=tangToennies, p=P.Bhh
         )
         if k
           E += _interTTM!(
-            F, u, p.μ, j, c1, Qh, Qc, 
-            Ach, Bch, Cch, A6ch; damp=tangToennies, p=Bch
+            F, u, P.μ, j, c1, P.Qh, P.Qc, 
+            P.Ach, P.Bch, P.Cch, P.A6ch; damp=tangToennies, p=P.Bch
           )
         end
 
@@ -106,31 +137,9 @@ function CH4(dv, v, u, p, t)
 end
 
 function CH4(F, G, y0, p)
-  D    = 4.0017  # eV
-  a    = 2.0415  # \AA^-1
-  req  = 1.0887  # \AA
-  K    = 3.33487 # eV rad^-1   76.9061 * 0.043363
-  θeq  = 1.87411 # rad 107.379 * (2pi/360)
-  Acc  = 1852.25 #
-  Ach  = 141.317 #
-  Ahh  = 112.503 #
-  Bcc  = 3.37925 # \AA^-1
-  Bch  = 3.25885 # \AA^-1
-  Bhh  = 4.05972 # \AA^-1
-  Ccc  = 13.15   #
-  Cch  = 4.51455 #
-  Chh  = 1.59497 #
-  Qh   = 0.51163 #
-  Qc   = - 4Qh   #
-  αh   = 0.38978 # \AA^3
-  αc   = 1.39327 # \AA^3
-  A6cc = αc^(2/6)
-  A6ch = (αc*αh)^(1/6)
-  A6hh = αh^(2/6)
 
   # initialize things
-  α      = repeat([αc, αh, αh, αh, αh], length(p.mols))
-  Q      = repeat([Qc, Qh, Qh, Qh, Qh], length(p.mols))
+  P      = p.potVars
   E      = 0.0
   u      = Vector[]
   forces = Vector[]
@@ -139,21 +148,21 @@ function CH4(F, G, y0, p)
     push!(forces, [0.0, 0.0, 0.0])
   end
 
-  # _getDipoles4TTM_Iterative!(p.μ, u, Q, α, p.mols)
-  _getDipoles4TTM_MatrixInversion!(p.μ, u, Q, α)
+  # _getDipoles4TTM_Iterative!(P.μ, u, Q, α, p.mols)
+  _getDipoles4TTM_MatrixInversion!(P.μ, u, P.Q, P.α)
 
-  E += _getDipolePolarizationEnergy(p.μ, α)
+  E += _getDipolePolarizationEnergy(P.μ, P.α)
 
   for mol in p.mols
     c, hs = mol[1], mol[2:5]
 
     for i in hs
-      E += _Morse!(forces, u, c, i, D, a, req)
+      E += _Morse!(forces, u, c, i, P.D, P.a, P.req)
     end
 
     for i in 1:4
       for j in i+1:4
-        E += _harmonicBondAngle!(forces, u, hs[i], c, hs[j], K, θeq)
+        E += _harmonicBondAngle!(forces, u, hs[i], c, hs[j], P.K, P.θeq)
       end
     end
     
@@ -164,25 +173,25 @@ function CH4(F, G, y0, p)
     c2, hs2 = par[2][1], par[2][2:5] 
 
     E += _interTTM!(
-      forces, u, p.μ, c1, c2, Qc, Qc, 
-      Acc, Bcc, Ccc, A6cc; damp=tangToennies, p=Bcc
+      forces, u, P.μ, c1, c2, P.Qc, P.Qc, 
+      P.Acc, P.Bcc, P.Ccc, P.A6cc; damp=tangToennies, p=P.Bcc
     )
 
     k = true
     for i in hs1
       E += _interTTM!(
-        forces, u, p.μ, i, c2, Qh, Qc, 
-        Ach, Bch, Cch, A6ch; damp=tangToennies, p=Bch
+        forces, u, P.μ, i, c2, P.Qh, P.Qc, 
+        P.Ach, P.Bch, P.Cch, P.A6ch; damp=tangToennies, p=P.Bch
       )
       for j in hs2
         E += _interTTM!(
-          forces, u, p.μ, i, j, Qh, Qh, 
-          Ahh, Bhh, Chh, A6hh; damp=tangToennies, p=Bhh
+          forces, u, P.μ, i, j, P.Qh, P.Qh, 
+          P.Ahh, P.Bhh, P.Chh, P.A6hh; damp=tangToennies, p=P.Bhh
         )
         if k
           E += _interTTM!(
-            forces, u, p.μ, j, c1, Qh, Qc, 
-            Ach, Bch, Cch, A6ch; damp=tangToennies, p=Bch
+            forces, u, P.μ, j, c1, P.Qh, P.Qc, 
+            P.Ach, P.Bch, P.Cch, P.A6ch; damp=tangToennies, p=P.Bch
           )
         end
 
--- src/md/simulation.jl
+++ src/md/simulation.jl
@@ -43,11 +43,11 @@ function runNVE(EoM, t1, t2, dt, bdys; save="full", kwargs...)
   pos   = [SVector{3}(i.r) for i in bdys]
   vel   = [SVector{3}(i.v) for i in bdys]
   mas   = [i.m for i in bdys]
-  μ     = [zeros(3) for i in bdys]
   tspan = (t1,t2)
 
+  potVars    = EoM(bdys)
   pars, mols = getPairs(bdys)
-  simu       = NVEsimu(bdys, pars, mols, [], [], save, mas, μ)
+  simu       = NVEsimu(bdys, pars, mols, [], [], save, mas, potVars)
 
   prob  = SecondOrderODEProblem(EoM, vel, pos, tspan, simu; kwargs...)
   solu  = solve(prob, VelocityVerlet(), dt=dt, dense=false, calck=false, save_start=false)
@@ -59,17 +59,13 @@ function runNVT(EoM, tspan, dt, bdys, thermostat, thermoInps; save="full", kwarg
   pos   = [SVector{3}(i.r) for i in bdys]
   vel   = [SVector{3}(i.v) for i in bdys]
   mas   = [i.m for i in bdys]
-  μ     = [zeros(3) for i in bdys]
 
+  potVars    = EoM(bdys)
   pars, mols = getPairs(bdys)
-  simu       = NVTsimu(bdys, pars, mols, [], [], [], save, mas, μ, thermostat, thermoInps)
+  simu       = NVTsimu(bdys, pars, mols, [], [], [], save, mas, potVars, thermostat, thermoInps)
 
   prob  = SecondOrderODEProblem(EoM, vel, pos, tspan, simu; kwargs...)
   solu  = solve(prob, VelocityVerlet(), dt=dt, dense=false, calck=false)
 
   return solu
 end
-
-#TODO:
-# - Make Simulation struct more robust
-# - Consider changing how bdys struct works
