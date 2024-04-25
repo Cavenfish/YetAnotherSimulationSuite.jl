58e20d10fa32261fa22011edddb4299590ca0414
Brian C. Ferrari <ferraribrian1996@gmail.com>
2024-04-25 15:16:54 +0200

    damping now includes derivative of function

--- src/md/potentials/funcs/damping.jl
+++ src/md/potentials/funcs/damping.jl
@@ -6,11 +6,17 @@ Notes:
     so they should follow the format foo(r, p).
     Where p can be a single value or an array with
     all additional damping parameters.
+  - All functions must return the energy and force
+    damping as a tuple like (f,fp).
 """
 
 function tangToennies(r, D)
   dr = D * r
   s  = [dr^i/factorial(i) for i = 0:6] |> sum
+  sp = [i*(dr)^(i-1)/factorial(i) for i = 0:6] |> sum
   
-  1 - (exp(-dr) * s)
+  f  = 1 - (exp(-dr) * s)
+  fp = D * exp(-dr) * s - exp(-dr) * sp
+
+  (f,fp)
 end
--- src/md/potentials/funcs/inter.jl
+++ src/md/potentials/funcs/inter.jl
@@ -97,8 +97,8 @@ function _longDisp(ri, rj, Cij; damp=nothing, p=nothing)
   # Get damping value if wanted
   damp != nothing ? d = damp(r, p) : d = 1 
 
-  E = -Cij / r^6 * d
-  F = 6 * E * rvec / r^2
+  E = -Cij / r^6 * d[1]
+  F = 6 * E * rvec / r^2 * d[2]
 
   E,F
 end
@@ -110,8 +110,8 @@ function _longDisp!(F, u, i, j, Cij; damp=nothing, p=nothing)
   # Get damping value if wanted
   damp != nothing ? d = damp(r, p) : d = 1
   
-  E     = -Cij / r^6 * d
-  f     = 6 * E * rvec / r^2
+  E     = -Cij / r^6 * d[1]
+  f     = 6 * E * rvec / r^2 * d[2]
   F[i] -= f
   F[j] += f
 
