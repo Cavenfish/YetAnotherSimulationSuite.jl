a4380d665caf093b9a022333096d42741136de0a
Brian C. Ferrari <ferraribrian1996@gmail.com>
2024-05-23 17:27:30 +0200

    still working on a few issues but close to getting things functional

---
+++ src/lib/MBX/libmbx.jl
@@ -0,0 +1,72 @@
+
+const libmbx = dlopen(joinpath(@__DIR__, "libmbx.so"), Libdl.RTLD_GLOBAL) 
+const E_MBX2JMD = 0.043 # kcal/mol --> eV
+
+# /**
+#  * Initializes the system in the heap.
+#  * @param[in] coords Pointer to the coordinates (size 3N)
+#  * @param[in] nat_monomers Pointer to an array of the number of atoms in each monomer
+#  * @param[in] at_names Pointer to an array with the atom names of all the whole system
+#  * @param[in] monomers Pointer to the list of monomer ids in your system
+#  * @param[in] nmon Number of monomers
+#  * @param[in] json_file Name of the json configuration file
+#  */
+# void initialize_system_py_(double* coords, int* nat_monomers, char** at_names, char** monomers, int* nmon,
+#                            char* json_file)
+
+function mbx_initialize_system(vars)
+  sym = dlsym(libmbx, :initialize_system_py_)
+
+  @ccall $sym(
+    vars.xyz::Ptr{Cdouble},
+    vars.num_ats::Ptr{Cint},
+    vars.at_nams::Ptr{Ptr{UInt8}},
+    vars.mon_nams::Ptr{Ptr{UInt8}},
+    vars.num_mon::Ref{Cint},
+    vars.json::Ptr{UInt8}
+  )::Cvoid
+end
+
+function mbx_initialize_system(xyz, num_ats, at_nams, mon_nams, num_mon, json)
+  sym = dlsym(libmbx, :initialize_system_py_)
+
+  @ccall $sym(
+    xyz::Ptr{Cdouble},
+    num_ats::Ptr{Cint},
+    at_nams::Ptr{Ptr{UInt8}},
+    mon_nams::Ptr{Ptr{UInt8}},
+    num_mon::Ref{Cint},
+    json::Ptr{UInt8}
+  )::Cvoid
+end
+
+function mbx_get_energy(xyz, nats)
+  sym = dlsym(libmbx, :get_energy_)
+
+  E   = Ref{Cdouble}(0)
+
+  @ccall $sym(xyz::Ptr{Cdouble}, nats::Ref{Cint}, E::Ref{Cdouble})::Cvoid
+
+  E[] * E_MBX2JMD
+end
+
+function mbx_get_energy_grad(xyz, nats, G)
+  sym = dlsym(libmbx, :get_energy_g_)
+
+  E   = Ref{Cdouble}(0)
+
+  @ccall $sym(
+    xyz::Ptr{Cdouble}, 
+    nats::Ref{Cint}, 
+    E::Ref{Cdouble},
+    G::Ptr{Cdouble}
+  )::Cvoid
+
+  E[] * E_MBX2JMD, G * E_MBX2JMD
+end
+
+function mbx_finalize_system()
+  sym = dlsym(libmbx, :finalize_system_)
+
+  @ccall $sym()::Cvoid
+end
---
+++ src/md/potentials/MBX.jl
@@ -0,0 +1,85 @@
+"""
+MBX potentials
+
+This allows uing all the potentials found within the MBX package.
+
+MBX GitHub:
+https://github.com/paesanilab/MBX/tree/master
+
+MBX Paper:
+https://pubs.aip.org/aip/jcp/article/159/5/054802/2904909/MBX-A-many-body-energy-and-force-calculator-for
+"""
+
+struct _MBX_PotVars <: PotVars
+  xyz
+  json
+  num_ats
+  at_nams
+  num_mon
+  mon_nams
+end
+
+function MBX(bdys::Vector{Atom}, json::String)
+  monomers = Dict()
+  monomers["nh4+"] = ["N","H","H","H","H"]
+  monomers["nh3"] = ["N","H","H","H"]
+  monomers["ch4"] = ["C","H","H","H","H"]
+  monomers["pf6-"] = ["P","F","F","F","F","F","F"]
+  monomers["co2"] = ["C","O","O"]
+  monomers["li"] = ["Li"]
+  monomers["na"] = ["Na"]
+  monomers["k"] = ["K"]
+  monomers["rb"] = ["Rb"]
+  monomers["cs"] = ["Cs"]
+  monomers["f"] = ["F"]
+  monomers["cl"] = ["Cl"]
+  monomers["br"] = ["Br"]
+  monomers["i"] = ["I"]
+  monomers["ar"] = ["Ar"]
+  monomers["he"] = ["He"]
+  monomers["h2"] = ["H","H"]
+  monomers["h2o"] = ["O","H","H"]
+  monomers["n2o5"] = ["O","N","N","O","O","O","O"]
+
+  xyz = [j for i in bdys for j in i.r]
+
+  at_nams = [string(i.s) for i in bdys]
+
+  mon_nams = String[]
+  num_ats  = Int64[]
+
+  tmp = values(monomers) |> (x -> length.(x)) |> unique |> sort |> reverse
+  
+
+  #TODO: add a break case to prevent infinite loop
+
+  a = 1
+  while a <= length(at_nams)
+    for i in tmp
+      a+i-1 > length(at_nams) && continue
+      cur_ats = at_nams[a:a+i-1]
+      for j in keys(monomers)
+        if monomers[j] == cur_ats
+          push!(mon_nams, j)
+          push!(num_ats, length(monomers[j]))
+          a += length(monomers[j])
+          break
+        end
+      end
+    end
+  end
+
+  num_mon = length(mon_nams)
+
+  sym = dlsym(libmbx, :initialize_system_py_)
+
+  # @ccall $sym(
+  #   xyz::Ptr{Cdouble},
+  #   num_ats::Ptr{Cint},
+  #   at_nams::Ptr{Ptr{UInt8}},
+  #   mon_nams::Ptr{Ptr{UInt8}},
+  #   num_mon::Ref{Cint},
+  #   json::Ptr{UInt8}
+  # )::Cvoid
+  _MBX_PotVars(xyz, json, num_ats, at_nams, num_mon, mon_nams)
+end
