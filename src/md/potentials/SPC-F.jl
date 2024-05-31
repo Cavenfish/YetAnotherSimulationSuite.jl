8c337074a4a4e8a9ccf81a7453e407fb4dc0e4a2
Brian C. Ferrari <ferraribrian1996@gmail.com>
2024-05-31 18:24:02 +0200

    fully working now

--- src/md/potentials/SPC-F.jl
+++ src/md/potentials/SPC-F.jl
@@ -24,6 +24,55 @@ SPCF(bdys::Vector{Atom}) = _SPCF_PotVars(
   0.5 * 2.959855
 )
 
+function SPCF(dv, v, u, p, t)
+
+  # initialize things
+  E = 0.0
+  F = zero(u)
+  P = p.potVars
+
+  for mol in p.mols
+    o, h1, h2 = mol
+
+    E += _harmonicBond!(F, u, o, h1, P.Kb, P.req)
+    E += _harmonicBond!(F, u, o, h2, P.Kb, P.req)
+    E += _harmonicBondAngle!(F, u, h1, o, h2, P.Kθ, P.θeq)
+  end
+
+  for par in p.pars
+    o1, h1, h2 = par[1]
+    o2, h3, h4 = par[2]
+
+    E += _vdw!(F, u, o1, o2, P.ϵ, P.σ)
+
+    E += _Coulomb!(F, u, o1, o2, P.Qo, P.Qo)
+
+    for i in [h1, h2]
+      for j in [h3, h4]
+        E += _Coulomb!(F, u, i, j, P.Qh, P.Qh)
+      end
+    end
+
+    for i in [h1,h2]
+      E += _Coulomb!(F, u, o2, i, P.Qo, P.Qh)
+    end
+
+    for i in [h3,h4]
+      E += _Coulomb!(F, u, o1, i, P.Qo, P.Qh)
+    end
+
+  end
+
+  dv .= F ./ p.m
+  if typeof(p) == NVTsimu
+    p.thermostat!(p.temp,dv, v, p.m, p.thermoInps)
+  end
+
+  push!(p.energy, E)
+  push!(p.forces, F)
+
+end
+
 function SPCF(F, G, y0, p)
 
   # initialize things
