926c0fc3e0c6e19675210100b5a83d030470f079
Brian C. Ferrari <ferraribrian1996@gmail.com>
2024-06-12 17:05:49 +0200

    participation ration now based on threshold

--- src/analysis/participationRatio.jl
+++ src/analysis/participationRatio.jl
@@ -2,21 +2,31 @@
 function getIPR(modes; N=2)
   m   = N * 3
   l   = size(modes)[2]
-  ipr = zeros(l)
+  n   = div(l, m)
+  ipr = zeros(n,l)  
 
   for i = 1:l
-    ρ = reshape(modes[:, i], (m, l))
-    a = 0.0
-    b = 0.0
-
-    for j = 1:l
-      v  = ρ[:, j]
-      a += dot(v, v)^2
-      b += dot(v, v)
-    end
+    ρ = reshape(modes[:, i], (m, n))
+    b = dot(modes[:, i], modes[:, i])^2
     
-    ipr[i] += a / (b^2)
+    for j = 1:n
+      v         = ρ[:, j]
+      ipr[j, i] = dot(v,v)^2 / b
+    end
+
   end
   
   ipr
+end
+
+function getPR(ipr; x=1.6e-5)
+  l  = size(ipr)[2]
+  pr = zeros(l)
+
+  for i = 1:l
+    n     = findall(e -> e > x, ipr[:, i]) |> length
+    pr[i] = n
+  end
+
+  pr
 end
