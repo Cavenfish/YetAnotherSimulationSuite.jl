02aa735b96d9f6deebe1e80a446f54383b902dbc
Brian C. Ferrari <ferraribrian1996@gmail.com>
2024-12-04 18:34:35 +0100

    adding new experiment script (not tested)

---
+++ extra/experiments/freqVersusEnergy.jl
@@ -0,0 +1,62 @@
+using TOML, JLD2
+
+"""
+Calculates the vibrational frequency at various
+vibrational energies.
+
+Example of Input Card
+-----------------------
+
+[Settings]
+EoM = "COCO"
+clus = "/home/brian/myStuff/clusters.jld2"
+json = "/home/brian/myStuff/jsonFiles/mol.json"
+modes = "/home/brian/myStuff/modes.jld2"
+Erange = [0.01, 0.01, 1.5]
+
+
+[Saving]
+data = "myData.jld2"
+"""
+function fve(inpFile::String)
+
+  # Read input card
+  inp = TOML.parsefile(inpFile)
+
+  # Load clusters and modes
+  clus  = jldopen(inp["clus"])
+  modes = jldopen(inp["modes"])
+
+  # Load EoM 
+  EoM = JMD.mkvar(expt["EoM"])
+
+  # Make energy range
+  a = inp["Erange"]
+  E = collect(a[1]:a[2]:a[3])
+
+  # Save energy range and init saving file
+  jldsave(inp["data"]; E)
+
+  for k1 in keys(clus)
+
+    bdys = clus[k1]
+    
+    # Open data file to write data in
+    # I close between bdys incase one crashes you have some data still
+    jldopen(inp["data"], "a+") do file
+
+      for k2 in keys(modes[k1])
+
+        m = modes[k1][k2]
+        ν = getFvE(MBX, bdys, E, m)
+
+        file["$(k1)/$(k2)"] = ν
+
+      end# k2
+
+    end# close file
+
+  end# k1
+
+
+end
