7faa203b138e0fb679c94bf84a4aecbce4c8583f
Brian C. Ferrari <ferraribrian1996@gmail.com>
2025-01-15 17:05:56 +0100

    add cell optimization routines

--- src/analysis/optimizations.jl
+++ src/analysis/optimizations.jl
@@ -42,6 +42,24 @@ function getNewBdys(bdys, res)
   new
 end
 
+function getNewBdys(bdys, x0, box)
+  N    = length(bdys)
+  new  = Atom[]
+  x0 .*= repeat(box, N)
+
+  for i in 1:3:N*3
+    j::UInt16 = (i+2) / 3
+
+    r = SVector{3}([x0[i], x0[i+1], x0[i+2]])
+    v = bdys[j].v
+    m = bdys[j].m
+    s = bdys[j].s
+    push!(new, Atom(r,v,m,s))
+  end
+
+  new
+end
+
 function opt(EoM, algo, bdys; kwargs...)
 
   x0, vars = prep4pot(EoM, bdys)
@@ -51,4 +69,22 @@ function opt(EoM, algo, bdys; kwargs...)
   optBdys  = getNewBdys(bdys, res)
 
   optBdys
+end
+
+function optCell(EoM, bdys, box0; kwargs...)
+
+  x0, vars = prep4pot(EoM, bdys)
+
+  for i = 1:3:length(x0)
+    x0[i]   /= box0[1]
+    x0[i+1] /= box0[2]
+    x0[i+2] /= box0[3]
+  end
+
+  convCrit = Optim.Options(; kwargs...)
+  res      = optimize(box -> EoM(box, x0), box0, NelderMead(), convCrit)
+  newBox   = res.minimizer
+  newBdys  = getNewBdys(bdys, x0, newBox)
+
+  newBdys, newBox
 end
