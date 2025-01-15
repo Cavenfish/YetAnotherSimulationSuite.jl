6a83475d820223e46e778e45c5606fad5f173330
Brian C. Ferrari <ferraribrian1996@gmail.com>
2025-01-15 16:59:42 +0100

    add saveName and symprec params

--- src/lib/Phonopy/phonopy.jl
+++ src/lib/Phonopy/phonopy.jl
@@ -2,9 +2,9 @@
 # Communication between phonopy and JMD goes through the phonopy yaml file.
 # Python should be used for all non-force calculations.
 
-function phonopy_addForces(yamlFile, forces)
+function phonopy_addForces(yamlFile, saveName, forces; symprec=1e-5)
   phonopy    = pyimport("phonopy")
-  obj        = phonopy.load(yamlFile)
+  obj        = phonopy.load(yamlFile, symprec=symprec)
   obj.forces = forces
-  obj.save()
+  obj.save(saveName)
 end
