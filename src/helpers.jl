bf652112a502a353a379349133971d6a80e97b54
Brian C. Ferrari <ferraribrian1996@gmail.com>
2025-02-28 13:13:55 +0100

    initial refactor

--- src/helpers.jl
+++ src/helpers.jl
@@ -11,25 +11,6 @@ function mkvar(x)
   return X
 end
 
-function translateBdys!(bdys, v)
-  for i in bdys
-    i.r .+= v
-  end
-end
-
-function diffDotSqrt(v2, v1)
-  rvec = v2 - v1
-  r    = sqrt(dot(rvec, rvec))
-  (r, rvec)
-end
-
-function getAngle(r1, r2)
-  x  = (dot(r1, r2)) / (norm(r1) * norm(r2))
-  x  = round(x, digits=15)
-  
-  acos(x)
-end
-
 function getAngleCO(co1, co2)
   r1 = co1[1].r - co1[2].r
   r2 = co2[1].r - co2[2].r
@@ -93,21 +74,6 @@ function getLastFrame!(cell::MyCell, solu)
 
 end
 
-function swapAtoms!(bdys, i, j)
-  a = bdys[i].r
-  b = bdys[j].r
-
-  bdys[i].r = b
-  bdys[j].r = a
-end
-
-function centerBdys!(bdys)
-  com = CoM(bdys)
-  for i in bdys
-    i.r -= com
-  end
-end
-
 function CoM(bdys)
   M = sum([i.m for i in bdys])
   r = sum([i.m*i.r for i in bdys])
@@ -153,146 +119,6 @@ function reducedMass(mas::Vector{Float64})
   return μ
 end
 
-function swapIso!(bdys, swap, mas)
-  for i in 1:length(swap)
-    j         = swap[i]
-    bdys[j].m = mas[i]
-  end
-end
-
-function vibExcite!(mol, eignvec, E)
-  M = [i.m for i in mol for j in 1:3]
-  v = @. sqrt( 2E / M ) * eignvec
-
-  vcom = vCoM(mol)
-  for i in 1:3:length(v)
-    j::UInt32 = (i+2)/3
-    mol[j].v += v[i:i+2]
-  end 
-
-  if !isapprox(vCoM(mol), vcom; atol=1e-8)
-    println("Uh Oh")
-    zeroVCoM!(mol)
-  end
-
-end
-
-function transExcite!(mol, KE)
-  r  = randVector()
-  ke = KE * 8.6173e-5 #convert Kelvin to eV
-
-  for i in mol
-    i.v += sqrt(2ke / i.m) .* r
-  end
-end
-
-function getTransEnergy(mol)
-  μ = reducedMass(mol)
-  v = vCoM(mol)
-  E = 0.5 * μ * dot(v,v)
-  return E
-end
-
-function getTransEnergy(pos,vel,mas)
-  μ = reducedMass(mas)
-  v = vCoM(vel,mas)
-  E = 0.5 * μ * dot(v,v)
-  return E
-end
-
-function getRotEnergy(mol)
-  vcom = vCoM(mol)
-  com  =  CoM(mol)
-  E    = 0.0
-
-  for i in mol
-    r  = i.r - com
-    v  = i.v - vcom
-    w  = cross(r,v) / dot(r,r)
-    I  = i.m * dot(r,r) 
-    E += 0.5 * I * dot(w,w)
-  end
-
-  return E
-end
-
-function getRotEnergy(pos,vel,mas)
-  vcom = vCoM(vel,mas)
-  com  =  CoM(pos,mas)
-  E    = 0.0
-
-  for i in 1:length(mas)
-    r  = pos[i] - com
-    v  = vel[i] - vcom
-    w  = cross(r,v) / dot(r,r)
-    I  = mas[i] * dot(r,r)
-    E += 0.5 * I * dot(w,w)
-  end
-  return E
-end
-
-function getVibEnergy(mol, eignvec; pot=nothing)
-
-  E = 0.0
-  for i in 1:3:length(eignvec)
-    j::UInt32 = (i+2)/3
-    ehat      = eignvec[i:i+2] / norm(eignvec[i:i+2])
-    E        += 0.5 * mol[j].m * dot(mol[j].v, ehat)^2
-  end
-
-  if pot != nothing
-    E += getPotEnergy(pot, mol)
-  end
-
-  return E
-end
-
-#The general method above will be great for more complicated molecules
-# but it is very slow. While below only works for CO but is fast.
-
-function getCOVibEnergy(mol; pot=nothing)
-  diff = mol[2].r - mol[1].r
-  rhat = normalize(diff)
-  v1   = dot(mol[1].v, rhat)
-  v2   = dot(mol[2].v, rhat)
-  E    = 0.5*mol[1].m*v1^2 + 0.5*mol[2].m*v2^2
-
-  if pot != nothing
-    E += getPotEnergy(pot, mol)
-  end
-
-  return E
-end
-
-function getCOVibEnergy(pos,vel,mas; pot=nothing)
-  diff = pos[2] - pos[1]
-  rhat = normalize(diff)
-  v1   = dot(vel[1], rhat)
-  v2   = dot(vel[2], rhat)
-  E    = 0.5*mas[1]*v1^2 + 0.5*mas[2]*v2^2
-
-  if pot != nothing
-    x0 = [j for i in pos for j in i]
-    E += pot(true, nothing, x0, optVars([[1,2]], []))
-  end
-
-  E
-end
-
-function getPotEnergy(EoM, bdys::Vector{MyAtoms})
-  x0, vars = prep4pot(EoM, bdys)
-  energy   = EoM(true, nothing, x0, vars)
-  
-  energy
-end
-
-function getPotEnergy(EoM, cell::MyCell)
-  x0, vars = prep4pot(EoM, cell)
-  energy   = EoM(true, nothing, x0, vars)
-  
-  energy
-end
-
 function getForces(EoM, bdys::Vector{MyAtoms})
   x0, vars = prep4pot(EoM, bdys)
   G        = zero(x0)
@@ -308,37 +134,3 @@ function getForces(EoM, cell::MyCell)
 
   [-G[i:i+2] for i = 1:3:length(G)]
 end
-
-function getSurfaceMolecules(bdys; α=nothing)
-  _, mols = getPairs(bdys)
-
-  pts  = [i.r for i in bdys]
-
-  A    = alphashape(pts; α=α)
-  
-  i = vcat(A.perimeter...) |> unique
-  j = findall.(e -> e in i, mols) |> (x -> findall(e -> !isempty(e), x))
-
-  surf = bdys[vcat(mols[j]...)]
-
-  surf
-end
-
-function pickRandomMol(bdys, loc)
-
-  surf = getSurfaceMolecules(bdys)
-  bulk = [i for i in bdys if !(i in surf)]
-
-  if loc == "surf"
-
-    _,mols = getPairs(surf)
-    return surf[rand(mols)]
-
-  elseif loc == "bulk"
-
-    _,mols = getPairs(bulk)
-    return bulk[rand(mols)]
-
-  end
-  
-end 
--- src/md/bodies.jl
+++ src/md/bodies.jl
@@ -1,5 +1,3 @@
-
-
 #Atoms in simulation
 #Needs mutable to swap masses on the fly
 mutable struct Atom <: MyAtoms
@@ -9,33 +7,64 @@ mutable struct Atom <: MyAtoms
   s::Char
 end
 
-function getMols(bdys::Vector{MyAtoms}, rmax; D=3)
-  r   = [i.r for i in bdys]
-
-  pts = hcat(r...)
-
-  ret = dbscan(pts[1:D, :], rmax)
-
-  [i.core_indices for i in ret.clusters]
+function translateBdys!(bdys, v)
+  for i in bdys
+    i.r .+= v
+  end
 end
 
-function getPairs(bdys::Vector{MyAtoms})
+function swapAtoms!(bdys, i, j)
+  a = bdys[i].r
+  b = bdys[j].r
 
-  # Get mols and N
-  mols = if length(bdys) <= 3
-    getMols(bdys, 1.5, D=length(bdys)-1) 
-  else
-    getMols(bdys, 1.5)
-  end
-  N    = size(mols)[1]
-
-  # Make all pairs
-  pars = Pair[]
-  for i in 1:N
-    for j in i+1:N
-      push!(pars, Pair(mols[i],mols[j]))
-    end
-  end
-
-  pars, mols
+  bdys[i].r = b
+  bdys[j].r = a
 end
+
+function centerBdys!(bdys)
+  com = CoM(bdys)
+  for i in bdys
+    i.r -= com
+  end
+end
+
+function swapIso!(bdys, swap, mas)
+  for i in 1:length(swap)
+    j         = swap[i]
+    bdys[j].m = mas[i]
+  end
+end
+
+function getSurfaceMolecules(bdys; α=nothing)
+  mols = getMols(bdys, 1.5)
+
+  pts  = [i.r for i in bdys]
+
+  A    = alphashape(pts; α=α)
+  
+  i = vcat(A.perimeter...) |> unique
+  j = findall.(e -> e in i, mols) |> (x -> findall(e -> !isempty(e), x))
+
+  surf = bdys[vcat(mols[j]...)]
+
+  surf
+end
+
+function pickRandomMol(bdys, loc)
+
+  surf = getSurfaceMolecules(bdys)
+  bulk = [i for i in bdys if !(i in surf)]
+
+  if loc == "surf"
+
+    mols = getMols(bdys, 1.5)
+    return surf[rand(mols)]
+
+  elseif loc == "bulk"
+
+    mols = getMols(bdys, 1.5)
+    return bulk[rand(mols)]
+
+  end
+  
+end 
