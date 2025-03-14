a84f9832c0a76bedce6fb3dca2995c0ef88ea268
Brian C. Ferrari <ferraribrian1996@gmail.com>
2025-03-14 13:19:30 +0100

    forgot to delete COCOff include

--- src/JMD.jl
+++ src/JMD.jl
@@ -142,7 +142,6 @@ module JMD
   include("./md/post-processing.jl")
 
   include("./md/potentials/MvHff.jl")
-  include("./md/potentials/COCOff.jl")
   include("./md/potentials/HGNN.jl")
   include("./md/potentials/TIP4P.jl")
   include("./md/potentials/SPC-F.jl")
