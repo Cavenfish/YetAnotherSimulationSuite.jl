e5b19b8b0b0506d4f19e089fea98cd3730b248ca
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-04-28 14:08:16 +0200

    change function name style

--- src/lib/SCMEf/libscmef.jl
+++ src/lib/SCMEf/libscmef.jl
@@ -1,11 +1,11 @@
-function scmef_get_dipole(bdys::Vector{MyAtoms})
+function scmef_getDipole(bdys::Vector{MyAtoms})
   pos = [i.r for i in bdys]
   lat = [100 0 0;0 100 0; 0 0 100]
 
   py"scmef_get_dipole"(pos, lat, pbc=false)
 end
 
-function scmef_get_dipole(cell::MyCell)
+function scmef_getDipole(cell::MyCell)
 
   pos = getPos(cell)
 
