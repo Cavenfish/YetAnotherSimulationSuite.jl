c5e7cd8f976180578189bc3fc7d520dc2a1f0ed2
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-05-02 17:44:27 +0200

    add functionality for user args in scmef

--- src/md/potentials/SCMEf.jl
+++ src/md/potentials/SCMEf.jl
@@ -13,21 +13,38 @@ https://pubs.acs.org/doi/full/10.1021/acs.jctc.2c00598
 """
 
 struct _SCMEf_PotVars <: JMD.PotVars
-  lat
+  lat::Vector{Float64}
+  kwargs::Dict
 end
 
-SCMEf(bdys::Vector{JMD.MyAtoms}) = _SCMEf_PotVars(ones(3)*1e3)
+function SCMEf(bdys::Vector{JMD.MyAtoms}) 
+  # grab args if user defined them
+  kwargs = if isdefined(Main, :SCMEf_USER_ARGS)
+    Main.SCMEf_USER_ARGS
+  else
+    Dict()
+  end
+
+  _SCMEf_PotVars(ones(3)*1e3, kwargs)
+end
 
 function SCMEf(cell::JMD.MyCell)
   lat = reshape(cell.lattice, 9)
+  
+  # grab args if user defined them
+  kwargs = if isdefined(Main, :SCMEf_USER_ARGS)
+    Main.SCMEf_USER_ARGS
+  else
+    Dict()
+  end
 
-  _SCMEf_PotVars(lat[[1,5,9]])
+  _SCMEf_PotVars(lat[[1,5,9]], kwargs)
 end
 
 function SCMEf(dv, v, u, p, t)
   all(p.PBC) ? pbc = true : pbc = p.PBC
 
-  E, f = py"scmef_get_energy_and_forces"(u, p.potVars.lat, NC=p.NC, pbc=pbc)
+  E, f = py"scmef_get_energy_and_forces"(u, P.lat; pbc=pbc, P.kwargs...)
 
   F    = eachrow(f) |> (x -> convert(Vector{Vector{Float64}}, x))
 
@@ -44,17 +61,16 @@ end
 function SCMEf(F, G, y0, p)
 
   # initialize things
-  E = 0.0
+  P = p.potVars
   u = [y0[i:i+2] for i = 1:3:length(y0)]
 
   all(p.PBC) ? pbc = true : pbc = p.PBC
 
   if G != nothing
-    e, f = py"scmef_get_energy_and_forces"(u, p.potVars.lat, NC=p.NC, pbc=pbc)
-    E    = e
+    E, f = py"scmef_get_energy_and_forces"(u, P.lat; pbc=pbc, P.kwargs...)
     G   .= transpose(-f) |> (x -> reshape(x, length(x)))
   else
-    E    = py"scmef_get_energy"(u, p.potVars.lat, NC=p.NC, pbc=pbc)
+    E    = py"scmef_get_energy"(u, P.lat; pbc=pbc, P.kwargs...)
   end
 
   if F != nothing
