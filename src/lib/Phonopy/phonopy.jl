901b2c90658055f4a9ee37c219bff8963a3baebd
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-05-06 18:40:08 +0200

    add func to pull disps dataset rather than built cells

--- src/lib/Phonopy/phonopy.jl
+++ src/lib/Phonopy/phonopy.jl
@@ -92,6 +92,41 @@ function phonopy_getDisplacements(
   phonon, cells
 end
 
+function phonopy_getDisplacementsDataset(
+  cell::MyCell, primitive, supercell; dist=0.02, symprec=1e-5)
+
+  # Imports
+  phonopy      = pyimport("phonopy")
+  PhonopyAtoms = phonopy.structure.atoms.PhonopyAtoms
+
+  unitcell = PhonopyAtoms(
+    symbols   = string(cell.symbols...), 
+    cell      = cell.lattice, 
+    positions = getPos(cell)
+  )
+
+  phonon   = phonopy.Phonopy(unitcell, 
+    supercell_matrix = supercell, 
+    primitive_matrix = primitive,
+    factor           = phonopy.units.VaspToEv,
+    symprec          = symprec
+  )
+
+  phonon.generate_displacements(distance=dist)
+
+  n     = hcat(supercell...) |> det
+  disps = Tuple[]
+  
+  for disp in phonon.displacement_dataset["first_atoms"]
+    i::Int = (disp["number"] / n) + 1
+    d      = disp["displacement"]
+    
+    push!(disps, (i,d))
+  end
+
+  phonon, disps
+end
+
 function phonopy_addForces(yamlFile, saveName, forces; symprec=1e-5)
   phonopy    = pyimport("phonopy")
   obj        = phonopy.load(yamlFile, symprec=symprec)
