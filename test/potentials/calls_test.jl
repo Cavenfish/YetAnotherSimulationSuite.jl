230b84f48617b731d4337b4d5240b42fe220da65
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-05-08 18:55:14 +0200

    add new tests file

---
+++ test/potentials/calls_test.jl
@@ -0,0 +1,45 @@
+
+function staticTest(EoM, f)
+  file = joinpath(@__DIR__, f)
+  bdys = readXyz(file)
+
+  getPotEnergy(EoM, bdys)
+  getForces(EoM, bdys)
+
+  true
+end
+
+function dynamicTest(EoM, f)
+  file = joinpath(@__DIR__, f)
+  bdys = readXyz(file)
+
+  runMD(MvHff, bdys, (0.0, 10fs), 1fs)
+
+  true
+end
+
+# This collection of tests simply calls a static 
+# and dynamics potential call. It does not test 
+# if the potentials perform correctly, rather it 
+# checks if any code changes breaks their calls. 
+@testset "Test Potential Function Calls" begin
+  co  = "../testingFiles/xyzFiles/co.xyz"
+  h2o = "../testingFiles/xyzFiles/h2o.xyz"
+  
+  # MvHff
+  @test staticTest(MvHff, co)
+  @test dynamicTest(MvHff, co)
+
+  # HGNN
+  @test staticTest(HGNN, co)
+  @test dynamicTest(HGNN, co)
+
+  # TIP4P
+  @test staticTest(TIP4P, h2o)
+  @test dynamicTest(TIP4P, h2o)
+
+  # SPCF
+  @test staticTest(SPCF, h2o)
+  @test dynamicTest(SPCF, h2o)
+
+end
