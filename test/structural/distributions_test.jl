2649e5181b674b88c5c4283a2ca5e43ffa47ca8c
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-06-04 19:14:47 +0200

    fix distribution tests

--- test/structural/distributions_test.jl
+++ test/structural/distributions_test.jl
@@ -3,13 +3,22 @@
   file = joinpath(@__DIR__, "../testingFiles/xyzFiles/iceIh.xyz")
   bdys = readSystem(file) |> makeBdys
 
+  # Molecule CoM RDF
+  x, y = rdf(bdys)
+  Ro   = x[findPeaks(y, min=0.04)]
+
+  @test isapprox(Ro[1], 2.70, atol=5e-2)
+  @test isapprox(Ro[2], 4.45, atol=5e-2)
+
+  # Oxygen atom RDF
   x, y = rdf(bdys, "O")
   Ro   = x[findPeaks(y, min=0.04)]
 
   @test isapprox(Ro[1], 2.70, atol=5e-2)
   @test isapprox(Ro[2], 4.45, atol=5e-2)
 
-  x, y = rdf(bdys, "O", "H")
+  # O-H RDF
+  x, y = rdf(bdys, "O" => "H")
   Ro   = x[findPeaks(y, min=0.04)]
 
   @test isapprox(Ro[1], 0.91, atol=5e-2)
