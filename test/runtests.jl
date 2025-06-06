963d9906670f22444c324c025bf739de68e897a2
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-06-06 12:56:07 +0200

    uncomment out tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,53 +3,53 @@ using Test
 using JMD
 using LinearAlgebra
 
-# @testset "Aqua.jl" begin
-#   Aqua.test_all(JMD)
-# end
+@testset "Aqua.jl" begin
+  Aqua.test_all(JMD)
+end
 
-# @testset "IO" begin
-#   include("./io_test.jl")
-# end
+@testset "IO" begin
+  include("./io_test.jl")
+end
 
-# @testset "Helpers" begin
-#   include("./helpers_test.jl")
-# end
+@testset "Helpers" begin
+  include("./helpers_test.jl")
+end
 
-# @testset "Cells" begin
-#   include("./md/cells_test.jl")
-# end
+@testset "Cells" begin
+  include("./md/cells_test.jl")
+end
 
-# @testset "Potentials" begin
+@testset "Potentials" begin
 
-#   @testset "Call Test" begin
-#     include("./potentials/calls_test.jl")
-#   end
+  @testset "Call Test" begin
+    include("./potentials/calls_test.jl")
+  end
   
-#   @testset "HGNN" begin
-#     include("./potentials/hgnn_test.jl")
-#   end
+  @testset "HGNN" begin
+    include("./potentials/hgnn_test.jl")
+  end
 
-#   @testset "MvHff" begin
-#     include("./potentials/cocoff_test.jl")
-#   end
+  @testset "MvHff" begin
+    include("./potentials/cocoff_test.jl")
+  end
 
-# end
+end
 
-# @testset "Math Toolkit" begin
+@testset "Math Toolkit" begin
 
-#   @testset "Peak Finding" begin
-#     include("./mathtk/peakFinding_test.jl")
-#   end
+  @testset "Peak Finding" begin
+    include("./mathtk/peakFinding_test.jl")
+  end
 
-# end
+end
 
-# @testset "Structural" begin
+@testset "Structural" begin
 
-#   @testset "Distributions" begin
-#     include("./structural/distributions_test.jl")
-#   end
+  @testset "Distributions" begin
+    include("./structural/distributions_test.jl")
+  end
 
-# end
+end
 
 @testset "Analysis" begin
 
