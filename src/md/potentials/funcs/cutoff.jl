39e03f383f6731ad92f0b7fd7c3d9808297ecfc2
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-09-01 17:15:47 +0200

    add another cutoff function

--- src/md/potentials/funcs/cutoff.jl
+++ src/md/potentials/funcs/cutoff.jl
@@ -34,3 +34,19 @@ function switchLR!(S::AbstractVector, r::Float64, rs::Float64, rc::Float64)
   S[1] = 1 - 10x^3 + 15x^4 - 6x^5
   S[2] = (-30x^2 + 60x^3 - 30x^4) / (rc-rs)
 end
+
+function switchAP!(S::AbstractVector, r::Float64, rs::Float64, rc::Float64)
+
+  if r < rs
+    S[1] = 1.0
+    S[2] = 0.0
+    return
+  elseif r > rc
+    S .= 0.0
+    return
+  end
+
+  x    = (r - rs) / (rc - rs)
+  S[1] = (2 * x^3) - (3 * x^2) + 1
+  S[2] = ((6 * x^2) - 6x ) / (rc - rs)
+end
