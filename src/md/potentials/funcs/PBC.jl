f2171286a7b22a3e4f2507ce9a2916e64c834b3f
Brian C. Ferrari <48806333+Cavenfish@users.noreply.github.com>
2025-09-09 18:00:48 +0200

    WIP: made pbcVec! a zero allocation function

--- src/md/potentials/funcs/PBC.jl
+++ src/md/potentials/funcs/PBC.jl
@@ -1,6 +1,14 @@
 
+struct _PBC_Cache{AV3D<:AbstractVector}
+  v::AV3D
+  r::AV3D
+end
+
+const _pbc_buf = _PBC_Cache(MVector{3}(zeros(3)), MVector{3}(zeros(3)))
+
 function pbcVec!(
-  rbuf::AbstractVector, ri::AV, rj::AV, lat::AbstractMatrix
+  rbuf::AbstractVector, ri::AV, rj::AV, lat::AbstractMatrix;
+  buf=_pbc_buf
 ) where {AV<:AbstractVector}
   @. rbuf = rj - ri
   d       = norm(rbuf)
@@ -9,19 +17,16 @@ function pbcVec!(
     return d
   end
 
-  v = zeros(3)
-  r = zeros(3)
-
   for i = -1:1
     for j = -1:1
       for k = -1:1
-        v .= (lat[1, :] * i) + (lat[2, :] * j) + (lat[3, :] * k)
-        r .= (rj .+ v) .- ri
-        di = norm(r)
+        buf.v .= (lat[1, :] * i) + (lat[2, :] * j) + (lat[3, :] * k)
+        buf.r .= (rj .+ buf.v) .- ri
+        di     = norm(buf.r)
 
         if di < d
           d     = di
-          rbuf .= r
+          rbuf .= buf.r
         end
       end
     end
